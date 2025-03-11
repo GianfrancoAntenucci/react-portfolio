04bd340ac2947f417c4cdae800e1fd5c7b798747
Gianfranco <25282809+ciaffo@users.noreply.github.com>
2025-03-11 11:56:32 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import './styles/index.css';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/">
       <div className="App">
         <Navigation />
         <main>
